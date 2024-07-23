a2485703dcbcafd1a52bbca80d371e1184eb89d2
AnaPaula7 <anasantos.rs7@gmail.com>
2024-07-23 19:02:25 -0300

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AppCalculadora
+Essa aplicação utiliza conceitos de html, bootstrap, css e js, para realizar construir uma calculadora que faz cálculos com as operações básicas.
+É útil para desenvolver os conceitos de função, DOM e manipulação de variáveis.
--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 function calcular(tipo, valor){
-    console.log(tipo, valor)
+    //verifica se o tipo é numeral ou ação
+    if(tipo === 'acao'){
+        if(valor === 'c'){ 
+            //se é ação clear vai limpar o visor (id resultado)
+            document.getElementById('resultado').value = '';
+        }
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+        if (valor === '='){
+           var valor_campo = eval(document.getElementById('resultado').value)
+           document.getElementById('resultado').value = valor_campo
+        }
+    }else if(tipo === 'numeral'){  //montar o numeral que será utilizado no cálculo
+        var valor_campo = document.getElementById('resultado').value; //variável que recebe o value
+        document.getElementById('resultado').value = valor_campo + valor; //o input com id resultado recebe o valor do numeral, que agora é uma string e concatena com outro valor que for selecionado
+        //document.getElementById('resultado').value += valor
+    }
 }
